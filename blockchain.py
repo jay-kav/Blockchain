969fdefcc27151f62415eddd5cf2ddbb44ac00a4
kavanj49 <jamie.kavanagh49@mail.dcu.ie>
2025-11-06 01:02:19 +0000

    Initiall blockchain set up

---
+++ blockchain.py
@@ -0,0 +1,18 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+        def new_block(self):
+            pass
+
+        def new_transcation(self):
+            pass
+
+        #static used to indicate that this method does not depend on the instance and is just a utility function living inside the class
+        @staticmethod
+        def hash(block):
+            pass
+
+        # Used to make a method act like an attribute provoding a getter method
+        @property
