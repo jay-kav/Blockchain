c7414c016f21fd6ca3a4840bd211a02d7e1b02d6
kavanj49 <jamie.kavanagh49@mail.dcu.ie>
2025-11-06 18:28:06 +0000

    Step 2: create a flask app

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,10 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
+from uuid import uuid4
+
+from flask import Flask
 
 class Blockchain(object):
     def __init__(self):
@@ -82,3 +86,56 @@ class Blockchain(object):
             guess = f'{last_proof}{proof}'.encode()
             guess_hash = hashlib.sha256(guess).hexdigest()
             return guess_hash[:4] == "0000"
+        
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', '')
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # Reward for finding the proof
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    block = blockchain.new_block(proof)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return json.dumps(response), 200
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return json.dumps(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return json.dumps(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
