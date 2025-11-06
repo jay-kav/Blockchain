d8c6e844e66b9f0dd13566e467b45331968b6a53
kavanj49 <jamie.kavanagh49@mail.dcu.ie>
2025-11-06 20:26:03 +0000

    Fully functionong blockchain

--- blockchain.py
+++ blockchain.py
@@ -3,90 +3,158 @@ import json
 from textwrap import dedent
 from time import time
 from uuid import uuid4
+import requests
 
-from flask import Flask
+from flask import Flask, jsonify, request
+from urllib.parse import urlparse
 
 class Blockchain(object):
     def __init__(self):
-        self.chain = []
         self.current_transactions = []
+        self.chain = []
+
         # Create the genesis block
-        self.new_block(previous_hash='1', proof=100)
+        self.new_block(previous_hash=1, proof=100)
+        self.nodes = set()
 
-        def new_block(self):
-            """Create a new Block in the Blockchain"""
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
 
-            block = {
-                'index': len(self.chain) + 1,
-                'timestamp': time(),
-                'transactions': self.current_transactions,
-                'proof': proof,
-                'previous_hash': previous_hash or self.hash(self.chain[-1]),
-            }
+        :param address: <str> Address of node. Eg. 'http://
+        """
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
 
-            # Reset the current list of transactions
-            self.current_transactions = []
-            self.chain.append(block)
-            return block
+    def new_block(self, proof, previous_hash=None):
+        """Create a new Block in the Blockchain"""
 
-        def new_transcation(self):
-            pass
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
 
         #static used to indicate that this method does not depend on the instance and is just a utility function living inside the class
-        @staticmethod
-        def hash(block):
-            """Creates a SHA-256 hash of a Block"""
-            # Make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
-            block_string = json.dumps(block, sort_keys=True).encode()
-            return hashlib.sha256(block_string).hexdigest()
+    @staticmethod
+    def hash(block):
+        """Creates a SHA-256 hash of a Block"""
+        # Make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
         
 
 
-        def new_transaction(self, sender, recipient, amount):
+    def new_transaction(self, sender, recipient, amount):
+        """
+            `Creates a new transaction to go into the next mined Block`
+            :param sender: <str> Address of the Sender
+            :param recipient: <str> Address of the Recipient
+            :param amount: <int> Amount
+            :return: <int> The index of the Block that will hold this transaction
             """
-             `Creates a new transaction to go into the next mined Block`
-             :param sender: <str> Address of the Sender
-             :param recipient: <str> Address of the Recipient
-             :param amount: <int> Amount
-             :return: <int> The index of the Block that will hold this transaction
-             """
 
-            self.current_transactions.append({
-                'sender': sender,
-                'recipient': recipient,
-                'amount': amount,
-            })
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    # Used to make a method act like an attribute provoding a getter method
+    @property
+    def last_block(self):
+        """Returns the last Block in the chain"""
+        return self.chain[-1]
     
-            return self.last_block['index'] + 1
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+            - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+            - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
 
-        # Used to make a method act like an attribute provoding a getter method
-        @property
-        def last_block(self):
-            """Returns the last Block in the chain"""
-            return self.chain[-1]
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+    
+    def valid_proof(self, last_proof, proof):
+        """ Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?"""
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+    
+    def valid_chain(self, chain):
+        """Determine if a given blockchain is valid"""
         
-        def proof_of_work(self, last_proof):
-            """
-            Simple Proof of Work Algorithm:
-             - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
-             - p is the previous proof, and p' is the new proof
-            :param last_proof: <int>
-            :return: <int>
-            """
+        last_block = chain[0]
+        current_index = 1
 
-            proof = 0
-            while self.valid_proof(last_proof, proof) is False:
-                proof += 1
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
 
-            return proof
-        
-        def valid_proof(self, last_proof, proof):
-            """ Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?"""
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
 
-            guess = f'{last_proof}{proof}'.encode()
-            guess_hash = hashlib.sha256(guess).hexdigest()
-            return guess_hash[:4] == "0000"
-        
+            last_block = block
+            current_index += 1
+
+        return True
+    
+    def resolve_conflicts(self):
+        """
+        This is our Consensus Algorithm, it resolves conflicts
+        by replacing our chain with the longest one in the network.
+        :return: <bool> True if our chain was replaced, False if not
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        # We're only looking for chains longer than ours
+        max_length = len(self.chain)
+
+        # Grab and verify the chains from all the nodes in our network
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if the length is longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Replace our chain if we discovered a new, valid chain longer than ours
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
 app = Flask(__name__)
 
 node_identifier = str(uuid4()).replace('-', '')
@@ -136,6 +204,40 @@ def full_chain():
         'length': len(blockchain.chain),
     }
     return json.dumps(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values= request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+    
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
